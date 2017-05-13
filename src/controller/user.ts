cfa4d6f53d14de2018656baef9a759cae2f07464
y0za <yoza.is12s@gmail.com>
2017-05-14 00:10:26 +0900

    Add fake implementations of UserController method

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,20 +1,56 @@
+import * as express from 'express';
 import {
+  Body,
   Controller,
   Get,
+  HttpStatus,
+  Param,
   Post,
+  Response,
 } from 'nest.js';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 @Controller('users')
 export class UserController {
+  private users: User[] = [
+    { id: 1, name: 'kaban' },
+    { id: 2, name: 'serval' },
+    { id: 3, name: 'raccoon' },
+    { id: 4, name: 'fennec' },
+  ];
+
   @Get()
-  index(): void {
+  index(@Response() res: express.Response): void {
+    res.status(HttpStatus.OK).json({
+      users: this.users,
+    })
   }
 
   @Get('/:id')
-  show(): void {
+  show(@Response() res: express.Response, @Param('id') id: string): void {
+    const user = this.users.find((u: User) => {
+      return u.id.toString() === id
+    });
+
+    res.status(HttpStatus.OK).json(user);
   }
 
   @Post()
-  create(): void {
+  create(@Response() res: express.Response, @Body('name') name: string): void {
+    const maxId = this.users.reduce((a: number, b: User) => {
+      return Math.max(a, b.id)
+    }, 0);
+    console.log(`name: ${name}`);
+    const user: User = {
+      id: maxId + 1,
+      name: name,
+    };
+    this.users.push(user);
+
+    res.status(HttpStatus.CREATED).json(user);
   }
 }
