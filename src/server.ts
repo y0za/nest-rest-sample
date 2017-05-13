407bb61a348f7e49fb8162a4289d9acf9724fe50
y0za <yoza.is12s@gmail.com>
2017-05-14 00:08:59 +0900

    Set body-parser to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { NestFactory } from 'nest.js';
 import { ApiModule } from './api-module';
 
+const instance = express();
+instance.use(bodyParser.json());
+
 const port = 3000;
-const app = NestFactory.create(ApiModule);
+const app = NestFactory.create(ApiModule, instance);
 
 app.listen(port, () => {
   console.log('Application listen on port: ', port);
