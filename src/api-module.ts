f83252282f0bc3476cc4dfaabeaf2338ff61af53
y0za <yoza.is12s@gmail.com>
2017-05-16 00:02:18 +0900

    Set UserService to ApiModule

--- src/api-module.ts
+++ src/api-module.ts
@@ -1,9 +1,13 @@
 import { Module } from 'nest.js';
 import { UserController } from './controller/user';
+import { UserService } from './service/user';
 
 @Module({
   controllers: [
     UserController,
+  ],
+  components: [
+    UserService,
   ],
 })
 export class ApiModule {
